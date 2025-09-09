98535ddc0bd06faa82384b4e02d9289c69f300e1
Hans Feliciano <hgvfeliciano10@gmail.com>
2025-09-09 15:20:46 -0600

    create a new array to test 5th problem and added code for 6th problem. modified css

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ const users = [
   { id: 10, name: "Padmé Amidala", age: 27 },
 ];
 
-// broken test data for exercise 6
 
 // 1. Print out the names of each character in the console, then render them in the HTML list with id "names-list"
-  // get the target element 
+
+// get the target element 
 const namesList = document.getElementById("names-list")
 
-  // log each of the character names in the console and in the html list
+// log each of the character names in the console and in the html list
 users.forEach(character => {
   namesList.innerHTML += `<li>${character.name}</li>`
   console.log(character.name);
@@ -28,10 +28,11 @@ users.forEach(character => {
 
 
 // 2. Print out the names of characters whose age is less than 40 in the console, then render them in the HTML list with id "young-characters-list"
-  // get the young char list element 
+
+// get the young char list element 
 const youngCharList = document.getElementById("young-characters-list")
 
-  // get each char with the age less than 40 then add to the html
+// get each char with the age less than 40 then add to the html
 users.forEach(character => { 
   if (character.age < 40) { 
     if (character.name === "") { 
@@ -43,10 +44,11 @@ users.forEach(character => {
 })
 
 // 3. Create a reusable function that takes any array and uses logic to render a list of character names in the HTML. Use this function to populate the list with id "function-list"
-  // get the function list element 
+
+// get the function list element 
 const funcList = document.getElementById("function-list")
 
-  // create the function for logging char names in the array and adding to the HTML 
+// create the function for logging char names in the array and adding to the HTML 
 let printCharNames = (array) => { 
   array.forEach(character => { 
       funcList.innerHTML += `<li>${character.name}</li>`
@@ -57,10 +59,11 @@ let printCharNames = (array) => {
 printCharNames(users); // call the function for checking console.log of char names
 
 // 4. Create a function that takes an array and an age threshold parameter. The function should only display characters whose age is below the given number. Render results in the list with id "age-filter-list"
-  // get the age filter list 
+  
+// get the age filter list 
 const ageFilterList = document.getElementById("age-filter-list")
-
-  // create the function for filtering the age and getting the age based on the array provided
+  
+// create the function for filtering the age and getting the age based on the array provided
 let filterCharByAge = (array, age) => { 
   array.forEach(character => { 
     if (character.age < age) { 
@@ -76,7 +79,21 @@ let filterCharByAge = (array, age) => {
 filterCharByAge(users, 40); // call the function for checking age validation for char under 40
 
 // 5. Add error handling to your functions that will log an error message using console.error() if any object doesn't have a "name" property. Display any error messages in the div with id "error-messages"
-  // get the errorhandling list and messages elements
+  
+  // create a new user list with errors to test 
+  const users2 = [
+    { id: 1, name: "Luke Skywalker", age: 23 },
+    { id: 2, name: "Darth Vader", age: 45 },
+    { id: 3, name: "Princess Leia", age: 23 },
+    { id: 4, name: "Obi-Wan Kenobi", age: 57 },
+    { id: 5, name: "", age: 900 },
+    { id: 6, name: "Han Solo", age: 32 },
+    { id: 7, name: "Chewbacca", age: 234 },
+    { id: 8, name: "", age: 33 },
+    { id: 9, name: "", age: 112 },
+    { id: 10, name: "Padmé Amidala", age: 27 },
+  ];
+// get the errorhandling list and messages elements
 const errorHandlingList = document.getElementById("error-handling-list")
 const errorMessages = document.getElementById("error-messages")
 
@@ -84,7 +101,7 @@ const errorMessages = document.getElementById("error-messages")
 let printErrorHandledCharNames = (array) => { 
   array.forEach(character => { 
     if (character.name === "") { 
-      errorMessages.innerHTML += `<li>Character with age ${character.age} does not have a name</li>`
+      errorMessages.innerHTML += `<p>Character with age ${character.age} does not have a name</p>`
       console.error("Character does not have a name"); // log console error if char name doesnt exist
     } else 
       errorHandlingList.innerHTML += `<li>${character.name}</li>`
@@ -92,15 +109,16 @@ let printErrorHandledCharNames = (array) => {
   })
 };
 
- printErrorHandledCharNames(users); // call the function for error handling function
+ printErrorHandledCharNames(users2); // call the function for error handling function
 
 // 6. Test your error handling by creating a second array that's intentionally broken (missing name properties) and passing it to your functions. Verify that your error handling works correctly and displays errors in the div with id "broken-array-errors"
 
   // Create test array with missing values for name
 const testUserArray = [
-  {id: 11, name: "", age: 27},
-  {id: 12, name: "", age: 28},
-  {id: 13, name: "", age: 100},
+  {id: 11, name: "yvana", age: 27},
+  {id: 12, name: "celine", age: 100},
+  {id: 13, name: "", age: 28},
+  {id: 13, name: "", age: 19},
 ]
 
   // get the brokenArray list and errors
@@ -108,3 +126,16 @@ const brokenArrayList = document.getElementById("broken-array-list");
 const brokenArrayErrors = document.getElementById("broken-array-errors");
 
   // testing the error handling of the function 
+let brokenArrayCharList = (array) => { 
+  array.forEach(character => { 
+    if (character.name === "") { 
+      brokenArrayErrors.innerHTML += `<p>Character with age ${character.age} does not have a name</p>`
+      console.error("Character does not have a name"); // log console error if char name doesnt exist
+    } else 
+      brokenArrayList.innerHTML += `<li>${character.name}</li>`
+      console.log(character.name);
+  })
+};
+
+  //testing the brokenArray function
+  brokenArrayCharList(testUserArray);
